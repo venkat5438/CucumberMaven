ac51e4c28162f99d464d2735d46e0fb8750ce450
venkat5438 <venkatramreddy108@gmail.com>
2017-08-08 12:57:31 -0400

    Cucumber maven project added

---
+++ src/main/java/stepdef/GmailElements.java
@@ -0,0 +1,18 @@
+package stepdef;
+
+import org.openqa.selenium.By;
+
+/**
+ * Created by breddy on 05-05-2017.
+ */
+public class GmailElements {
+
+    //static By ClickSignIn= By.xpath("html/body/nav/div/a[2]");
+    static By UserName= By.xpath("//*[@id='identifierId']");
+    static By NextButton = By.xpath("//*[@id='identifierNext']/content/span");
+    static By PassWord = By.xpath("//*[@id='password']/div[1]/div/div[1]/input");
+
+
+
+
+}
